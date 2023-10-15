209e5c9b8def481242dd0f50cc887a460a4ed7f5
robertson-utexas <120421665+robertson-utexas@users.noreply.github.com>
2023-10-14 23:17:56 -0500

    push

---
+++ fa23IdentityTemplate/fa23IdentityTemplate/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+//TODO: Make this namespace match your project name
+namespace fa23IdentityTemplate.Models
+{
+    public class AppUser : IdentityUser
+    {
+        //TODO: Add custom user fields - first name is included as an example
+        [Display(Name = "First Name")]
+        public String FirstName { get; set; }
+
+    }
+}
